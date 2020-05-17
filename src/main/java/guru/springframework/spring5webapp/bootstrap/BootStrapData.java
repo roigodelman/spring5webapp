4e5493358ed5af1ab5b8110985cd13eb5f00517c
roigodelman <roi1godelman@gmail.com>
2020-05-17 16:06:22 -0400

    Repo

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,43 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private  final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Eve");
+        Book ddd = new Book("ddd", "2132");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "John");
+        Book jee = new Book("J2EE", "1231546");
+        rod.getBooks().add(jee);
+        jee.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(jee);
+
+        System.out.println("Started Bootstrap");
+        System.out.println("Number of books: " + bookRepository.count());
+    }
+}
